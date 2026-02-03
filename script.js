0d8f870599f597ca432351fbb927bdc0852ce016
Lorena Piñones Huerta <lfpinones@uc.cl>
2026-02-03 02:00:25 -0300

    Update script.js
    Se actualizó con evento que corrige error de evento anterior y permite el cambio de estilos claro y oscuro.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // cosas requeridas por la actividad: 3 eventos
 $(document).ready(function(){
+    //cambio de modos de color
+    $("#modos").click(function(){
+            $("body").toggleClass("bg-dark text-white");
+            $("header").toggleClass("bg-success");
+            $(".col").toggleClass("bg-dark");
+            $(this).toggleClass("btn-secondary text-white btn-outline-secondary text-dark");
 
 
-$("#modos").click(function(){
-    $("body").toggleClass("dark dark0");
+        });
+
 });
 
 
-});
+//accidentalmente cree el efecto en el tema claro, no pregunten cómo lo hice//
+
